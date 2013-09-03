928702790f4c03c6b80e46ce236ad258448befae
zhongdeliu <zhongdeliu@googlemail.com>
2013-09-03 08:25:40 +0200

    added minMax helper

---
+++ javascript/minMax.js
@@ -0,0 +1,14 @@
+/**
+ * Determines if a number is within [min, max] and returns it (or the capped values)
+ * @param  {number} value Value to check
+ * @param  {number} min   Minimum value
+ * @param  {number} max   Maximum value
+ * @return {number} capped value
+ */
+function minMax(value, min, max) {
+    value = Number(value);
+    min = Number(min);
+    max = Number(max);
+
+    return (value <= min) ? min : (value >= max) ? max : value;
+}
